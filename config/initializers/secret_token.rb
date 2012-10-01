341d87e48e9cb0cde1dfa41bf919e883c8a8a0b2
Guido Knips <Guido.Knips@googlemail.com>
2012-10-01 13:47:30 +0200

    initial commit for stublog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stublog::Application.config.secret_token = '5fe8bf986f6bb8b67906b7176619c386543e694dcb51e80f3db74983e57aecbf83e5ded94eedb2ad8a345246248a4c1f0c51fefc6475415060869aeb04e4a35a'
