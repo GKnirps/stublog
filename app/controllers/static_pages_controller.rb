af8de28c383a6b059ad2e5847dde8a1e6afbf378
Guido Knips <Guido.Knips@googlemail.com>
2012-10-28 18:57:28 +0100

    fixed displaying wrong quote on homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
   	@blogposts = Blogpost.limit(10)
-	@quote_of_the_day = QuoteOfTheDay.first
+	@quote_of_the_day = QuoteOfTheDay.published.first
   end
 
   def contact
