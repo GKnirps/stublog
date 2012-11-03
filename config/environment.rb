02a49e7a56884ad99f3c8c496d7ff14adfa0511f
Guido Knips <Guido.Knips@googlemail.com>
2012-11-03 18:26:49 +0100

    added restart of rufus scheduler if phusion passenger forks (otherwise rufus scheduler does not last long)

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,23 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Stublog::Application.initialize!
+
+require 'rufus/scheduler'
+#when running with passenger, restart rufus scheduler if needed
+if defined?(PhusionPassenger) then
+	PhusionPassenger.on_event :starting_worker_process do |forked|
+	if forked then
+		scheduler = Rufus::Scheduler.start_new
+
+		if Rails.env.development? then
+		        schedule = '* * * * *'
+		else
+		        schedule = '0 0 * * *'
+		end
+		scheduler.cron schedule do
+		        if QuoteOfTheDay.publish? then
+		                QuoteOfTheDay.publish_next!
+		        end
+		end	
+	end
+end
