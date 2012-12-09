567b07deb74a1f9808e032d1e966905d05f6b643
Guido Knips <Guido.Knips@googlemail.com>
2012-12-09 14:39:38 +0100

    added workaround for removing tag-like structures by the rbbcode gem instead of escaping them

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,6 +18,12 @@ module ApplicationHelper
 
 	#default bbcode parser (I plan to extend this one)
 	def bbparse(text, safe=:safe)
+		#workaround: since the new version, everything is passed through the sanitizer gem
+		#as result, '<' and '>' are not escaped anymore, if they appear to be part of a tag
+		#so escape them right here:
+		text = text.gsub(/</, "&lt;").gsub(/>/, "&gt;")
+
+		#parse the bbcode
 		bbparser = RbbCode.new
 		htmlcode = bbparser.convert(text)
 		return htmlcode.html_safe if safe == :safe
